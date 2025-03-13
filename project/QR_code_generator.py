d1eff0c34fabd8f5977011dab7c7d79f6b4ed21c
bhagyalakshmiP03 <bhagyarani2004@gmail.com>
2025-03-13 11:05:28 +0530

    generated bhagya_qrcode

---
+++ project/QR_code_generator.py
@@ -0,0 +1,21 @@
+import qrcode
+
+def generate_qr_code(data, file_name):
+    qr = qrcode.QRCode(
+        version=1,
+        # error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(file_name)
+    print(f"QR code saved as {file_name}")
+    return img
+
+data = input("Enter the data to be stored in QR code: ")
+file_name = input("Enter the name of the file to save the QR code: ")  #
+generate_qr_code(data, file_name)
+# Output: QR code saved as qr_code.png or jpg or jpeg
+
